5fd91cd5fea75fb1c1af41432595c701f3eb5009
bimrely <bim@bimrely.com>
2024-08-03 15:53:47 +0100

    first commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", //
+    origin: "https://frontend-jh2ijters-bimrelys-projects.vercel.app/", //
     methods: ["GET", "POST","PUT"]
   }
 });
