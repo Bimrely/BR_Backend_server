929f4a50b45fb8c1a9df37672d31edd8243e069a
Bimrely <bim@bimrely.com>
2024-10-16 00:12:48 +0500

    wip-commit

--- Socket/socket.js
+++ Socket/socket.js
@@ -1,29 +1,29 @@
-import { Server } from 'socket.io';
+// import { Server } from 'socket.io';
 
-let io;
+// let io;
 
-function initialize(server) {
-  io = new Server(server);
+// function initialize(server) {
+//   io = new Server(server);
 
-  io.on('connection', (socket) => {
-    console.log('A user connected');
+//   io.on('connection', (socket) => {
+//     console.log('A user connected');
 
-    socket.on('disconnect', () => {
-      console.log('A user disconnected');
-    });
-  });
-}
+//     socket.on('disconnect', () => {
+//       console.log('A user disconnected');
+//     });
+//   });
+// }
 
-function emitLikeNotification(articleId) {
-  io.emit('likeNotification', { articleId });
-}
+// function emitLikeNotification(articleId) {
+//   io.emit('likeNotification', { articleId });
+// }
 
-function emitCommentNotification(articleId) {
-  io.emit('commentNotification', { articleId });
-}
+// function emitCommentNotification(articleId) {
+//   io.emit('commentNotification', { articleId });
+// }
 
-export {
-  initialize,
-  emitLikeNotification,
-  emitCommentNotification,
-};
+// export {
+//   initialize,
+//   emitLikeNotification,
+//   emitCommentNotification,
+// };
