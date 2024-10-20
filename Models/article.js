8b65b1810ccf7ce497762c3bc113a3933a7c8954
Bimrely <bim@bimrely.com>
2024-10-20 19:43:30 +0500

    wip-commit

--- Models/article.js
+++ Models/article.js
@@ -1,112 +1,160 @@
 
 import mongoose, { Schema } from 'mongoose';
 
-const filesSchema = new Schema({
-    name: String,
-    fileInfo: Object,
-    fileBucket: String,
-    fileKey: String,
-    location: String,
-    contentType: String,
-    added: { type: Date, default: Date.now },
+
+
+
+const CommentSchema = new mongoose.Schema({
+  commentAuthor: {  // Use commentAuthor to store the Profile reference
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Profile",  // Reference the Profile model
+  },
+  text: String,
+  likes: { type: Number, default: 0 },
+  likedBy: [
+    {
+      userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    },
+  ],
+  file: filesSchema,
+  replies: [ReplySchema],  // Assuming you have a Reply schema
+  added: { type: Date, default: Date.now },
 });
 
-const ReplySchema = new Schema({
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
+const ArticleSchema = new mongoose.Schema({
+  text: String,
+  file: {
+    type: String,  // Store the URL or file path
+    default: '',
+  },
+  createdAt: { type: Date, default: Date.now },
+  author: { 
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Profile',  // Reference to the Profile model
+  },
+  likes: { type: Number, default: 0 },
+  shares: { type: Number, default: 0 },
+  comments: [CommentSchema],  // Embed the comment schema
+  likedBy: [
+    {
+      userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     },
-    
-    likes: { type: Number, default: 0 },
-    // likedBy: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-    likedBy: [
-        {
-        userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-          firstName: String,
-          lastName: String,
-        },
-      ],
-    text: String,
-    firstName: String,
-    lastName: String,
-    added: { type: Date, default: Date.now },
-  //   replies: [{ 
-  //     type: mongoose.Schema.Types.ObjectId, 
-  //     ref: 'Reply' 
-  // }],
-    // replies: [{       
-    //   text:String,
-    // firstName: String,
-    // lastName: String,}],
-
-});
-
-
-const Reply = mongoose.model('Reply', ReplySchema);
-
-const CommentSchema = new Schema({
-  commentAuthor: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Profile",
-    },
-    text: String,
-    firstName: String,
-    lastName: String,
-    likes: { type: Number, default: 0 },
-    // likedBy: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-    likedBy: [
-        {
-        userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-          firstName: String,
-          lastName: String,
-        },
-      ],
-    file:filesSchema,
-    replies: [ReplySchema],
-    added: { type: Date, default: Date.now }
-});
-
-const ArticleSchema = new Schema({
-  firstName:String,
-  lastName:String,
-    text: String,
-    // file: filesSchema,
-    file:{
-      type: String, // Store the URL or file path of the profile picture
-      default: '',
-
-    },
-    createdAt: { type: Date, default: Date.now },
-    author: { 
-      type: Schema.Types.ObjectId, 
-      ref: 'Profile'  // Reference the Profile model, not User
-    },
-    likes: { type: Number, default: 0 },
-    shares: { type: Number, default: 0 },
-    comments: [CommentSchema],
-    profilePicture: {
-      type: String, // Store the URL or file path of the profile picture
-      default: '', // You can set a default profile picture if you want
-    },
-    
-    likedBy: [
-        {
-          userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-          firstName: String,
-          lastName: String,
-        },
-      ],
-  
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-    }
+  ],
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',  // Reference to the User model
+  },
 });
 
 export const Article = mongoose.model('Article', ArticleSchema);
 
 
-export const ReplyModel = Reply;
+// const filesSchema = new Schema({
+//     name: String,
+//     fileInfo: Object,
+//     fileBucket: String,
+//     fileKey: String,
+//     location: String,
+//     contentType: String,
+//     added: { type: Date, default: Date.now },
+// });
+
+// const ReplySchema = new Schema({
+//     userId: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: "User",
+//     },
+    
+//     likes: { type: Number, default: 0 },
+//     // likedBy: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+//     likedBy: [
+//         {
+//         userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+//           firstName: String,
+//           lastName: String,
+//         },
+//       ],
+//     text: String,
+//     firstName: String,
+//     lastName: String,
+//     added: { type: Date, default: Date.now },
+//   //   replies: [{ 
+//   //     type: mongoose.Schema.Types.ObjectId, 
+//   //     ref: 'Reply' 
+//   // }],
+//     // replies: [{       
+//     //   text:String,
+//     // firstName: String,
+//     // lastName: String,}],
+
+// });
+
+
+// const Reply = mongoose.model('Reply', ReplySchema);
+
+// const CommentSchema = new Schema({
+//   commentAuthor: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: "Profile",
+//     },
+//     text: String,
+//     firstName: String,
+//     lastName: String,
+//     likes: { type: Number, default: 0 },
+//     // likedBy: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+//     likedBy: [
+//         {
+//         userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+//           firstName: String,
+//           lastName: String,
+//         },
+//       ],
+//     file:filesSchema,
+//     replies: [ReplySchema],
+//     added: { type: Date, default: Date.now }
+// });
+
+// const ArticleSchema = new Schema({
+//   firstName:String,
+//   lastName:String,
+//     text: String,
+//     // file: filesSchema,
+//     file:{
+//       type: String, // Store the URL or file path of the profile picture
+//       default: '',
+
+//     },
+//     createdAt: { type: Date, default: Date.now },
+//     author: { 
+//       type: Schema.Types.ObjectId, 
+//       ref: 'Profile'  // Reference the Profile model, not User
+//     },
+//     likes: { type: Number, default: 0 },
+//     shares: { type: Number, default: 0 },
+//     comments: [CommentSchema],
+//     profilePicture: {
+//       type: String, // Store the URL or file path of the profile picture
+//       default: '', // You can set a default profile picture if you want
+//     },
+    
+//     likedBy: [
+//         {
+//           userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+//           firstName: String,
+//           lastName: String,
+//         },
+//       ],
+  
+//     userId: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: "User",
+//     }
+// });
+
+// export const Article = mongoose.model('Article', ArticleSchema);
+
+
+// export const ReplyModel = Reply;
 
 
 // import mongoose, { Schema } from 'mongoose';
