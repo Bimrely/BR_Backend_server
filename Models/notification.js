d8f06545d4e676485c44480b41bb85e017f482fe
Bimrely <bim@bimrely.com>
2024-10-26 00:26:33 +0500

    wip-commit

--- Models/notification.js
+++ Models/notification.js
@@ -9,7 +9,7 @@ const notificationSchema = new Schema({
   type: {
     type: String,
     required: true,
-    enum: ['like', 'comment', 'reply', 'share'] // Add all possible notification types here
+    enum: ['like', 'comment', 'reply', 'share' ,'mention'] // Add all possible notification types here
   },
   article: { type: Schema.Types.ObjectId, ref: 'Article', },
   job:{ type: Schema.Types.ObjectId, ref: 'Job', },
